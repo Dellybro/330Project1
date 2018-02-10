1dff616fa390f295ae1cc091708759194452624d
FugenApps <Dellybro@Traviss-MacBook-Pro.local>
2018-02-09 23:04:19 -0700

    test

--- tcb.h
+++ tcb.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ucontext.h>
+// #include <ucontext.h>
 
 struct Queue {
     struct TCB_t *head;
@@ -16,9 +16,9 @@ typedef struct TCB_t {
 } TCB_t;
 
 void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size){
-    memset(tcb, '\0', sizeof(TCB_t));       // wash, rinse
-    getcontext(&tcb->context);              // have to get parent context, else snow forms on hell
-    tcb->context.uc_stack.ss_sp = stackP;
-    tcb->context.uc_stack.ss_size = (size_t) stack_size;
-    makecontext(&tcb->context, function, 0);// context is now cooked
+    // memset(tcb, '\0', sizeof(TCB_t));       // wash, rinse
+    // getcontext(&tcb->context);              // have to get parent context, else snow forms on hell
+    // tcb->context.uc_stack.ss_sp = stackP;
+    // tcb->context.uc_stack.ss_size = (size_t) stack_size;
+    // makecontext(&tcb->context, function, 0);// context is now cooked
 }
--- threads.h
+++ threads.h
@@ -5,13 +5,18 @@
 struct Queue *RunQ;
 
 void start_thread(void (*function)(void)){
-
+// begin pseudo code
+//      allocate a stack (via malloc) of a certain size (choose 8192)
+//      allocate a TCB (via malloc)
+//      call init_TCB with appropriate arguments
+//      call addQ to add this TCB into the “RunQ” which is a global header pointer
+//end pseudo code
 }
 
 void run(){
-    ucontext_t parent; // get a place to store the main context, for faking
-    getcontext(&parent); // magic sauce
-    swapcontext(&parent, &(RunQ->head->context)); // start the first thread
+    // ucontext_t parent; // get a place to store the main context, for faking
+    // getcontext(&parent); // magic sauce
+    // swapcontext(&parent, &(RunQ->head->context)); // start the first thread
 }
 
 void yield(){
